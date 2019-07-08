2dd49c545a7eac48a5c1ac84860b78003a858015
陈亮 <1109107755@qq.com>
2019-07-08 15:32:55 +0800

    学生信息管理系统环境搭建及配置文件已完成，且创建了第一个控制器类(主页控制器)，已通过测试。

--- src/main/java/cn/edu/hunter/sms/web/controller/SystemController.java
+++ src/main/java/cn/edu/hunter/sms/web/controller/SystemController.java
@@ -1,10 +1,26 @@
 package cn.edu.hunter.sms.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
+ * 系统主页控制器
+ *
  * @version 1.0.0
  * @className SystemController
  * @anthor Administrator
  * @data 2019/7/8
  */
+@Controller
+@RequestMapping("/system")
 public class SystemController {
+
+    @RequestMapping(path = "/index",method = RequestMethod.GET)
+    public ModelAndView index(ModelAndView mv) {
+        mv.setViewName("hello world");
+        mv.addObject("user","it猎人工作室");
+        return mv;
+    }
 }
--- src/main/java/cn/edu/hunter/sms/web/interceptor/LoginInterceptor.java
+++ src/main/java/cn/edu/hunter/sms/web/interceptor/LoginInterceptor.java
@@ -1,10 +1,12 @@
 package cn.edu.hunter.sms.web.interceptor;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+
 /**
  * @version 1.0.0
  * @className LoginInterceptor
  * @anthor Administrator
  * @data 2019/7/4
  */
-public class LoginInterceptor {
+public class LoginInterceptor{
 }
